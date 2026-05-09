4d0c24375d3ac3f5dfaeb3281410db208b0a36e2
saul <Saul.constructor25@gmail.com>
2026-05-09 10:54:32 -0400

    Official Router v7.1: Mapping /, /dashboard, /login

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import AukenOptica from './pages/AukenOptica'
+import AukenOpticaDashboard from './pages/AukenOpticaDashboard'
+import AukenLogin from './pages/AukenLogin'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const ProtectedRoute = ({ children }) => {
+  const auth = localStorage.getItem("auken_auth");
+  return auth === "true" ? children : <Navigate to="/login" />;
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<AukenLogin />} />
+        <Route path="/" element={<ProtectedRoute><AukenOptica /></ProtectedRoute>} />
+        <Route path="/dashboard" element={<ProtectedRoute><AukenOpticaDashboard /></ProtectedRoute>} />
+        {/* Redirección por defecto */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-);
+)
